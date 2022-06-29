03af42be1610ab7ecd306d06cebe70a88e0a8214
Luiz Fernando Romanini <lfromanini@yahoo.com>
2022-06-29 14:43:24 -0300

    enhancements

--- sshWrapper.sh
+++ sshWrapper.sh
@@ -11,34 +11,34 @@
 # Host *.local
 # 	LocalCommand	sshpass -f path/to/fileContainingThePassword
 
-function __sshWrapper::ssh()
+function __sshwrapper::ssh()
 {
-	local readonly SSH=$( whereis -b ssh | command awk '{ print $2 }' )
+	local readonly SSH=$( whereis -b ssh | awk '{ print $2 }' )
 
 	local args=""
 	local argType=""
 	local argPass=""
 
-	args=$( ${SSH} "$@" -F ~/.ssh/sshpass -G 2>/dev/null | command grep --max-count=1 --ignore-case "LocalCommand \+sshpass" | command awk '{ $1=$2="" ; print $0 }' | command sed 's/^[[:blank:]]*//' )
+	args=$( "${SSH}" "$@" -F ~/.ssh/sshpass -G 2>/dev/null | grep --max-count=1 --ignore-case "LocalCommand \+sshpass" | awk '{ $1=$2="" ; print $0 }' | sed 's/^[[:blank:]]*//' )
 
 	if [ -z "${args}" ] ; then
 
 		# no sshpass arguments set in ~/.ssh/sshpass file
-		${SSH} "$@"
+		"${SSH}" "$@"
 
 	else
 
 		argType="${args:0:2}"
-		argPass=$( echo "${args:2}" | command sed 's/^[[:blank:]]*//' )
+		argPass=$( echo "${args:2}" | sed 's/^[[:blank:]]*//' )
 
 		# variable expansion
 		if [ "${argType}" = "-f" ] ; then argPass=$( eval echo "${argPass}" ) ; fi
 
-		command sshpass "${argType}""${argPass}" ${SSH} "$@" -o PreferredAuthentications=password
+		command sshpass "${argType}""${argPass}" "${SSH}" "$@" -o PreferredAuthentications=password
 	fi
 }
 
 # replace ssh only if sshpass is available
-[ -z "$( whereis -b sshpass | command awk '{ print $2 }' )" ] && echo "Can't use sshWrapper : missing sshpass" && return 1
+[ -z "$( whereis -b sshpass | awk '{ print $2 }' )" ] && echo "Can't use sshWrapper : missing sshpass" && return 1
 
-alias ssh="__sshWrapper::ssh"
+alias ssh="__sshwrapper::ssh"
